199f41b0166f55d51fc48f27cc18e2b275d772b3
Jason Fauchelle <jason@mindscape.co.nz>
2013-06-28 15:36:23 +1200

    Old uninstall script experiments.

--- uninstall.ps1
+++ uninstall.ps1
@@ -5,23 +5,32 @@ Function Clean ($localPath)
 {
     $xml = New-Object xml
 
-	# load config as XML
 	$xml.Load($localPath)
-	$parentNode = $xml.SelectSingleNode("configuration")
-	$nodes = $xml.SelectNodes("configuration" + "/" + "RaygunSettings")
-	$i = 0
-	while($i -lt $nodes.Count)
+	$configNode = $xml.SelectSingleNode("configuration")
+	$allsections = $xml.SelectNodes("configuration/configSections/section")
+	Write-Host $allSections.Count + " sections found"
+	foreach($section in $allSections)
 	{
-	  $parentNode.RemoveChild($nodes.Item($i))
-	  $i++
+		Write-Host $section.GetAttribute("name")
 	}
-	$xml.Save($localPath)
+	$sections = $xml.SelectNodes("configuration/configSections/section") | where { $_.name -eq "RaygunSettings" }
+	if ($sections.Count -eq 0)
+	{
+		$nodes = $xml.SelectNodes("configuration/RaygunSettings")
+		$i = 0
+		while($i -lt $nodes.Count)
+		{
+		  $configNode.RemoveChild($nodes.Item($i))
+		  $i++
+		}
+		$xml.Save($localPath)
+	}
 }
 
-$configs = $project.ProjectItems | where {$_.Name -eq "Web.config" -or $_.Name -eq "App.config" }
+$configs = $project.ProjectItems | where { $_.Name -eq "Web.config" -or $_.Name -eq "App.config" }
 Foreach ($config in $configs)
 {
-    $localPath = $config.Properties | where {$_.Name -eq "LocalPath"}
-	Write-Host "LoaclPath: " + $localPath.Value
+	Write-Host "Uninstalling"
+    $localPath = $config.Properties | where { $_.Name -eq "LocalPath" }
     Clean $localPath.Value
 }
