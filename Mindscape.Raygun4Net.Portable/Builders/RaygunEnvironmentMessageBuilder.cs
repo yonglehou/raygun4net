7bbf06d34d0499e75722a604a8942e64440e3f52
Jason Fauchelle <jfauchelle@raygun.io>
2016-02-02 16:56:30 +1300

    Environment message builder stub

---
+++ Mindscape.Raygun4Net.Portable/Builders/RaygunEnvironmentMessageBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mindscape.Raygun4Net.Messages;
+
+namespace Mindscape.Raygun4Net.Builders
+{
+  public class RaygunEnvironmentMessageBuilder
+  {
+    public static RaygunEnvironmentMessage Build()
+    {
+      RaygunEnvironmentMessage message = new RaygunEnvironmentMessage();
+
+      return message;
+    }
+  }
+}
